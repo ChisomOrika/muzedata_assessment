f6f952bb441b75a92fa477c3442923d7a2938ddd
Orika Chisom <90322381+ChisomOrika@users.noreply.github.com>
2023-04-28 01:51:01 +0100

    Update test_dag.py

--- test_dag.py
+++ test_dag.py
@@ -1,8 +1,12 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-file_path_1 = '/opt/airflow/dags/testfile.py'
 
+# Define the file paths for the two Python scripts
+file_path_1 = '/opt/airflow/dags/test_save_csv.py'
+file_path_2 = '/opt/airflow/dags/test_check_csv.py'
+
+# Define the default arguments for the DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -11,17 +15,27 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+# Define the DAG
 dag = DAG(
-    'exchange_values',
+    'exchange_values_save_csv',
     default_args=default_args,
     description='Exchange the values of x and y every 5 minutes',
     schedule_interval=timedelta(minutes=5),
 )
 
+# Define the BashOperator for task 1, which runs the Python script that saves output to a CSV file
 t1 = BashOperator(
     task_id='exchange_values',
     bash_command=f'python {file_path_1}',
     dag=dag,
 )
 
-t1
+# Define the BashOperator for task 2, which runs the Python script that checks if the CSV file is saved
+t2 = BashOperator(
+    task_id='check_csv_file',
+    bash_command=f'python {file_path_2}',
+    dag=dag,
+)
+
+# Set the dependency between tasks, so that task 2 runs after task 1 completes
+t1 >> t2
